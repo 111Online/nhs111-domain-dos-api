26c4a0f822ecb76f21e722adde9c22e91aa3056c
james.tiffen@nhs.net <james.tiffen@nhs.net>
2019-06-05 14:55:22 +0100

    Removed automapper ref and used extension method

--- NHS111.Domain.Dos.Api/Models/Response/DosService.cs
+++ NHS111.Domain.Dos.Api/Models/Response/DosService.cs
@@ -226,7 +226,7 @@ namespace NHS111.Domain.Dos.Api.Models.Response
                 ; if (!IsDateInList(date, nextWeeksDates))
                     continue;
 
-                var dayOfWeek = Mapper.Map<DayOfWeek>(date.DayOfWeek);
+                var dayOfWeek = date.DayOfWeek.ToResponseDayOfWeek();
 
                 short startTimeHours;
                 short startTimeMinutes;
@@ -373,5 +373,13 @@ namespace NHS111.Domain.Dos.Api.Models.Response
         public TimeSpan OpeningTime { get; set; }
         public TimeSpan ClosingTime { get; set; }
         public DayOfWeek Day { get; set; }
+    }
+
+    public static class DayOfWeekExtensions
+    {
+        public static DayOfWeek ToResponseDayOfWeek(this System.DayOfWeek value)
+        {
+            return (DayOfWeek)(int)value;
+        }
     }
 }
